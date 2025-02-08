2d00e042e8d3246524f5c0d197682427b3e5aca3
gabriel-wargha <gabriel@wargha.com>
2025-02-08 16:29:12 -0700

    CreditCard homework

--- creditCard/card.js
+++ creditCard/card.js
@@ -1,15 +1,20 @@
+let errorMsg = '';
+
 function isCardNumberValid(number) {
-	// normally we would contact a credit card service...but we don't know how to do that yet. So to keep things simple we will only accept one number
 	return number === '1234123412341234';
 }
+
 function displayError(msg) {
 	// display error message
 	document.querySelector('.errorMsg').innerHTML = msg;
 }
+
 function submitHandler(event) {
 	event.preventDefault();
+	cardNumber = document.querySelector('#cardNumber');
+
 	let errorMsg = '';
-	console.log(this.cardNumber.value);
+
 	// clear any previous errors
 	displayError('');
 	// check credit card number
@@ -19,12 +24,33 @@ function submitHandler(event) {
 	} else if (!isCardNumberValid(this.cardNumber.value)) {
 		// it is a number, but is it valid?
 		errorMsg += 'Card number is not a valid card number\n';
+	} else if (!isDateValid(event)) {
+		errorMsg = 'The expiration date must be in the future';
 	}
 	if (errorMsg !== '') {
 		// there was an error. stop the form and display the errors.
 		displayError(errorMsg);
 		return false;
 	}
+
+	errorMessage = '';
+	return true;
+}
+
+function isDateValid(event) {
+	event.preventDefault();
+	let inputMonth = parseInt(document.getElementById('month').value) - 1;
+	let inputYear = parseInt(document.getElementById('year').value);
+
+	let userDate = new Date(inputYear, inputMonth, 1);
+
+	let today = new Date();
+	today.setHours(0, 0, 0, 0);
+
+	if (userDate < today) {
+		return false;
+	}
+
 	return true;
 }
 
