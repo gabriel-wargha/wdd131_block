7c7f582bf0ec4c0c6c06a36b6314f40680c6665e
gabriel-wargha <gabriel@wargha.com>
2025-02-22 10:05:52 -0700

    changes_images

--- A_Final_website/script.js
+++ A_Final_website/script.js
@@ -18,37 +18,37 @@ const posts = [
 		id: 1,
 		title: 'Growth mindset',
 		paragraph: 'A growth mindset can change your life',
-		img_src: './Images/Blog_1.jpg',
+		img_src: './Images/Blog_1.png',
 	},
 	{
 		id: 2,
 		title: 'How to become a leader',
 		paragraph: 'There are 5 steps to develop a leadership ability',
-		img_src: './Images/blog2.jpg',
+		img_src: './Images/blog2.png',
 	},
 	{
 		id: 3,
 		title: 'Fear vs faith',
 		paragraph: 'Do you know the difference between fear and faith?',
-		img_src: './Images/Blog_image_3.jpg',
+		img_src: './Images/Blog_image_3.png',
 	},
 	{
 		id: 4,
 		title: 'Why do you feel lazy?',
 		paragraph: 'Little changes in your habits can help you to have more energy',
-		img_src: './Images/blog_image_4.jpg',
+		img_src: './Images/blog_image_4.png',
 	},
 	{
 		id: 5,
 		title: 'Work smart',
 		paragraph: 'What is the importance of working smart?',
-		img_src: './Images/blog_image_5.jpg',
+		img_src: './Images/blog_image_5.png',
 	},
 	{
 		id: 6,
 		title: 'Mental health',
 		paragraph: '3 Ways to improve your mental health',
-		img_src: './Images/blog_image_6.jpg',
+		img_src: './Images/blog_image_6.png',
 	},
 ];
 
